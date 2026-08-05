a18d4a6c290fb52bbcfc308cafdaad83485d6d8e
Evan Hao <jiyang250@gmail.com>
2026-08-05 10:22:32 -0700

    Add AllowCredentials to CORS policy in Program.cs

--- src/SecondChance.WebApi/Program.cs
+++ src/SecondChance.WebApi/Program.cs
@@ -139,7 +139,8 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(allowedOrigins)
               .AllowAnyHeader()
-              .AllowAnyMethod();
+              .AllowAnyMethod()
+              .AllowCredentials();
     });
 });
 
